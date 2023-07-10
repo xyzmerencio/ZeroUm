14dd36df5766c9805287cd02ae827c1cbedea25e
vitormluz <vitormdluzz2003@outlook.com>
2023-07-10 11:43:06 -0300

    update dirbrute

--- src/dirbrute/dirbrute.py
+++ src/dirbrute/dirbrute.py
@@ -1,44 +1,19 @@
 import requests
-import threading
 import sys
 
 
 def dirbrute(url, wordlist):
-    session = requests.Session()
-    lock = threading.Lock()
-    total_requests = 0
-
-    def fazer_request(url):
+    for word in wordlist:
+        word = word.strip()
+        url_final = f"{url}/{word}"
+        response = requests.get(url_final)
+        response = response.status_code
         try:
-            response = session.get(url)
-            response_code = response.status_code
-            return response_code
+            requests.get(url_final)
+            if response != 404:
+                print(url_final, response)
         except Exception as error:
             print(error)
-
-    def print_resultado(url):
-        nonlocal total_requests
-        response_code = fazer_request(url)
-        if response_code and response_code != 404:
-            with lock:
-                print(url, response_code)
-            total_requests -= 1
-
-    try:
-        for word in wordlist:
-            word = word.strip()
-            url_final = f"{url}/{word}"
-            thread = threading.Thread(target=lambda: print_resultado(url_final))
-            thread.start()
-            total_requests += 1
-
-            while total_requests >= 3:
-                pass
-
-        for thread in threading.enumerate():
-            if thread != threading.current_thread():
-                thread.join()
-
-    except KeyboardInterrupt:
-        print("\n\n\n Tchau :)")
-        sys.exit(0)
+        except KeyboardInterrupt:
+            print("\n\n\n Tchau :)")
+            sys.exit(0)
